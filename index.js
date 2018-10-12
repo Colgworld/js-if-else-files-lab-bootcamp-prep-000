aadb9da89940a69209b77a973b97449a035fb812
Michael W. Colgan <michaelwcolgan@gmail.com>
2018-10-12 16:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ var name = "Susan"
 var height = "74"
 
 
-if (name === "Susan") {
-  console.log("The name is correct.")
-}
-  else {
-    console.log("Incorrect.")
-}
-
-
 // Don't worry about this
 module.exports = { name, height
 }
