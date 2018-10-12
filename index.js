af1726008f6401cc72fb7c1fdc445da2203bb974
Michael W. Colgan <michaelwcolgan@gmail.com>
2018-10-12 16:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct.")
 }
-//   else {
-//     console.log("Incorrect.")
-// }
+  else {
+    console.log("Incorrect.")
+}
 
 
 // Don't worry about this
